920441c9c48d29dd8ed9eff0b1f1693e209882d3
Matt <mw4854@nyu.edu>
2022-11-20 19:30:16 -0500

    redirect build

--- docs/_app/immutable/chunks/1-7c84937d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-f1174f19.js";export{t as component};
--- docs/_app/immutable/chunks/2-6c6a7718.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-be7e9c59.js";export{t as component};
